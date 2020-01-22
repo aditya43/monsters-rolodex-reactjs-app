5e89445b26c7acf5b2ef44e862bf1d33b2f6793c
Aditya Hajare <aditya43@gmail.com>
2020-01-22 18:00:35 +0530

    Monsters Rolodex App deployed to github pages

---
+++ build/precache-manifest.3b6e97f6b8650616c6065c0cc727e91f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fac747b63e7bf9a878c566e4ca964885",
+    "url": "/monsters-rolodex-reactjs-app/index.html"
+  },
+  {
+    "revision": "912e585dc0d728658ad0",
+    "url": "/monsters-rolodex-reactjs-app/static/css/main.7dc1ea8f.chunk.css"
+  },
+  {
+    "revision": "08a6a6075503df4cd79d",
+    "url": "/monsters-rolodex-reactjs-app/static/js/2.b56b2ec7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex-reactjs-app/static/js/2.b56b2ec7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "912e585dc0d728658ad0",
+    "url": "/monsters-rolodex-reactjs-app/static/js/main.daad09de.chunk.js"
+  },
+  {
+    "revision": "1564e617c9d96eb15bc3",
+    "url": "/monsters-rolodex-reactjs-app/static/js/runtime-main.d650322a.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
+
+import './App.css';
+
+class App extends Component {
+    constructor () {
+        super();
+
+        this.state = {
+            monsters: [],
+            searchField: ''
+        };
+    }
+
+    async componentDidMount () {
+        try {
+            const response = await fetch('https://jsonplaceholder.typicode.com/users');
+            const users = await response.json();
+            this.setState({ monsters: users });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    handleChange = (e) => {
+        this.setState({ searchField: e.target.value });
+    }
+
+    render () {
+        const { monsters, searchField } = this.state;
+        const filteredMonsters = monsters.filter(
+            monster => monster.name.toLowerCase().includes(searchField.toLowerCase())
+        );
+
+        return (
+            <div className="App">
+                <h1>Monsters Rolodex</h1>
+                <SearchBox
+                    placeholder='Search Monsters..'
+                    handleChange={this.handleChange}
+                />
+                <CardList monsters={filteredMonsters} />
+            </div>
+        );
+    }
+}
+
+export default App;
